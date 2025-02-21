4cec9377e5a3c46029cb41d8db1d5085d9188a5c
bin2810 <bin200528@gmail.com>
2025-02-21 16:26:01 +0700

    up addcart

--- asset/js/bars.js
+++ asset/js/bars.js
@@ -1,34 +1,29 @@
 $(function () {
-    // Mở sidebar khi bấm nút #bars
     $('#bars').click(function () {
         $(".nav-category").addClass('show-nav');
     });
 
-    // Đóng sidebar khi bấm ngoài
     $(document).click(function (event) {
         if ($(".nav-category").hasClass("show-nav") && !$(event.target).closest(".nav-category, #bars").length) {
             $(".nav-category").removeClass("show-nav");
         }
     });
 
-    // Hàm xử lý dropdown theo kích thước màn hình
     function handleDropdown() {
         if (window.innerWidth <= 1070) {
-            $(".dropdown").hide(); // Ẩn dropdown khi nhỏ hơn 1024px
+            $(".dropdown").hide(); 
             $('.menu-item').off("click").on("click", function () {
                 $(".dropdown").not($(this).next()).slideUp();
                 $(this).next(".dropdown").slideToggle();
             });
         } else {
-            $(".dropdown").css("display", ""); // Reset về CSS mặc định để tránh lỗi
-            $('.menu-item').off("click"); // Xóa sự kiện click khi màn hình lớn
+            $(".dropdown").css("display", ""); 
+            $('.menu-item').off("click"); 
         }
     }
 
-    // Gọi hàm kiểm tra ngay khi tải trang
     handleDropdown();
 
-    // Kiểm tra lại khi resize màn hình
     $(window).resize(function () {
         handleDropdown();
     });
