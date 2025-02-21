1a5182f9d79bfb5f56e9cada285b4db32dd372ff
bin2810 <bin200528@gmail.com>
2025-02-21 12:11:41 +0700

    up banenr

--- asset/js/navbar.js
+++ asset/js/navbar.js
@@ -1,10 +1,14 @@
 window.addEventListener("scroll", function() {
     var navbar = document.querySelector(".navbar");
+    var banner = document.querySelector(".banner");
     if (window.scrollY > 1) { 
         navbar.style.position = "fixed";
         navbar.style.top = "0rem";
+        banner.style.margin = "0";
     } else {
         navbar.style.top = "5rem";
+        banner.style.margin = "6rem 0";
+
     }
 });
 
